331e5e55250ef7bd565ba9072e5ef1f0833a83b5
Daniel Nilsson <daniel.nilsson@appgate.com>
2020-05-06 14:12:11 +0200

    ringfence attributes.
    Signed-off-by: Daniel Nilsson <daniel.nilsson@appgate.com>

--- appgate/resource_appgate_ringfence_rule.go
+++ appgate/resource_appgate_ringfence_rule.go
@@ -1,6 +1,7 @@
 package appgate
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/hashicorp/terraform/helper/schema"
@@ -35,6 +36,95 @@ func resourceAppgateRingfenceRule() *schema.Resource {
 				Type:        schema.TypeString,
 				Description: "Name of the object.",
 				Required:    true,
+			},
+
+			"notes": {
+				Type:        schema.TypeString,
+				Description: "Notes for the object. Used for documentation purposes.",
+				Default:     DefaultDescription,
+				Optional:    true,
+			},
+
+			"tags": {
+				Type:        schema.TypeSet,
+				Description: "Array of tags.",
+				Optional:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+
+			"actions": {
+				Type:     schema.TypeSet,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+
+						"protocol": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: func(v interface{}, name string) (warns []string, errs []error) {
+								s := v.(string)
+								list := []string{"icmp", "icmpv6", "udp", "tcp"}
+								for _, x := range list {
+									if s == x {
+										return
+									}
+								}
+								errs = append(errs, fmt.Errorf("protocol must be on of %v, got %s", list, s))
+								return
+							},
+						},
+
+						"direction": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: func(v interface{}, name string) (warns []string, errs []error) {
+								s := v.(string)
+								list := []string{"up", "down"}
+								for _, x := range list {
+									if s == x {
+										return
+									}
+								}
+								errs = append(errs, fmt.Errorf("direction must be on of %v, got %s", list, s))
+								return
+							},
+						},
+
+						"action": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: func(v interface{}, name string) (warns []string, errs []error) {
+								s := v.(string)
+								list := []string{"allow", "block"}
+								for _, x := range list {
+									if s == x {
+										return
+									}
+								}
+								errs = append(errs, fmt.Errorf("action must be on of %v, got %s", list, s))
+								return
+							},
+						},
+
+						"hosts": {
+							Type:     schema.TypeSet,
+							Required: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+
+						"ports": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+
+						"types": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+					},
+				},
 			},
 		},
 	}
