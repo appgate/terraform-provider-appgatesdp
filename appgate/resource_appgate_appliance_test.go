e6ae696dc7b8a001533d8ebf43c09b4ed3671d4c
Daniel Nilsson <daniel.nilsson@appgate.com>
2020-06-29 12:06:33 +0200

    corrected appliance ipv4 dhcp configuration.
    - Updated indentation on test stub, to match terraform fmt.
    - Updated testcase with 2 nics.
    - Corrected bug from copypaste  error on ipv4  dhcp routers, and ntp.
    
    Signed-off-by: Daniel Nilsson <daniel.nilsson@appgate.com>

--- appgate/resource_appgate_appliance_test.go
+++ appgate/resource_appgate_appliance_test.go
@@ -38,28 +38,39 @@ func TestAccApplianceBasicController(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "client_interface.3426664317.proxy_protocol", "true"),
 
 					resource.TestCheckResourceAttr(resourceName, "networking.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.dns_domains.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.dns_domains.112524683", "aa.com"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.dns_domains.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.dns_domains.112524683", "aa.com"),
 
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.dns_servers.#", "2"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.dns_servers.251826590", "1.1.1.1"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.dns_servers.2609393598", "8.8.8.8"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.dns_servers.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.dns_servers.251826590", "1.1.1.1"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.dns_servers.2609393598", "8.8.8.8"),
 
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.name", "eth0"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.ipv4.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.ipv4.3519857096.dhcp.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.ipv4.3519857096.dhcp.2319808068.dns", "true"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.ipv4.3519857096.dhcp.2319808068.enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.ipv4.3519857096.dhcp.2319808068.ntp", "true"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.nics.0.ipv4.3519857096.dhcp.2319808068.routers", "true"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.name", "eth0"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.ipv4.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.ipv4.3519857096.dhcp.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.ipv4.3519857096.dhcp.2319808068.dns", "true"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.ipv4.3519857096.dhcp.2319808068.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.ipv4.3519857096.dhcp.2319808068.ntp", "true"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.0.ipv4.3519857096.dhcp.2319808068.routers", "true"),
 
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.routes.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.routes.0.address", "0.0.0.0"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.routes.0.gateway", "1.2.3.4"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.routes.0.netmask", "24"),
-					resource.TestCheckResourceAttr(resourceName, "networking.1914549515.routes.0.nic", "eth0"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv4.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv4.964719136.dhcp.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv4.964719136.dhcp.156169847.dns", "false"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv4.964719136.dhcp.156169847.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv4.964719136.dhcp.156169847.ntp", "false"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv4.964719136.dhcp.156169847.routers", "false"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv4.964719136.static.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.ipv6.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.nics.1.name", "eth1"),
+
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.routes.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.routes.0.address", "0.0.0.0"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.routes.0.gateway", "1.2.3.4"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.routes.0.netmask", "24"),
+					resource.TestCheckResourceAttr(resourceName, "networking.129643110.routes.0.nic", "eth0"),
 
 					resource.TestCheckResourceAttr(resourceName, "snmp_server.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "snmp_server.10952821.enabled", "true"),
@@ -192,198 +203,212 @@ func testAccCheckApplianceDestroy(s *terraform.State) error {
 func testAccCheckApplianceBasicController() string {
 	return fmt.Sprintf(`
 data "appgate_site" "default_site" {
-	   site_name = "Default site"
+  site_name = "Default site"
 }
 
 resource "appgate_appliance" "test_controller" {
-	name     = "controller-test"
-	hostname = "envy-10-97-168-1337.devops"
+  name     = "controller-test"
+  hostname = "envy-10-97-168-1337.devops"
 
-	client_interface {
-        hostname = "envy-10-97-168-1337.devops"
-        proxy_protocol = true
-        https_port     = 444
-        dtls_port      = 445
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-        override_spa_mode = "TCP"
-	}
+  client_interface {
+    hostname       = "envy-10-97-168-1337.devops"
+    proxy_protocol = true
+    https_port     = 444
+    dtls_port      = 445
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
+    }
+    override_spa_mode = "TCP"
+  }
 
-	peer_interface {
-		hostname   = "envy-10-97-168-1337.devops"
-		https_port = "1337"
-	}
-	tags = [
-		"terraform",
-		"api-test-created"
-	]
-	ntp {
-		servers {
-			hostname = "0.ubuntu.pool.ntp.org"
-		}
-		servers {
-			hostname = "1.ubuntu.pool.ntp.org"
-		}
-		servers {
-			hostname = "2.ubuntu.pool.ntp.org"
-		}
-		servers {
-			hostname = "3.ubuntu.pool.ntp.org"
-		}
-	}
-	networking {
+  peer_interface {
+    hostname   = "envy-10-97-168-1337.devops"
+    https_port = "1337"
+  }
+  tags = [
+    "terraform",
+    "api-test-created"
+  ]
+  ntp {
+    servers {
+      hostname = "0.ubuntu.pool.ntp.org"
+    }
+    servers {
+      hostname = "1.ubuntu.pool.ntp.org"
+    }
+    servers {
+      hostname = "2.ubuntu.pool.ntp.org"
+    }
+    servers {
+      hostname = "3.ubuntu.pool.ntp.org"
+    }
+  }
+  networking {
 
-		hosts {
-		  hostname = "bla"
-		  address  = "0.0.0.0"
-		}
+    hosts {
+      hostname = "bla"
+      address  = "0.0.0.0"
+    }
 
-		nics {
-		  enabled = true
-		  name    = "eth0"
-		  ipv4 {
-			dhcp {
-			  enabled = true
-			  dns     = true
-			  routers = true
-			  ntp     = true
-			}
-		  }
-		}
-		dns_servers = [
-		  "8.8.8.8",
-		  "1.1.1.1",
-		]
-		dns_domains = [
-		  "aa.com"
-        ]
-        routes {
-            address = "0.0.0.0"
-            netmask = 24
-            gateway = "1.2.3.4"
-            nic = "eth0"
+    nics {
+      enabled = true
+      name    = "eth0"
+      ipv4 {
+        dhcp {
+          enabled = true
+          dns     = true
+          routers = true
+          ntp     = true
         }
-	}
-	controller {
-		enabled = true
+      }
     }
-    snmp_server {
-        enabled    = true
-        tcp_port   = 161
-        udp_port   = 161
-        snmpd_conf = "foo"
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-    }
-    healthcheck_server {
-        enabled = true
-        port    = 5555
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-    }
-    prometheus_exporter {
-        enabled = true
-        port    = 1234
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-    }
-    ping {
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-    }
-    log_forwarder {
-        enabled = true
-        elasticsearch {
-            url = "https://aws.com/elasticsearch/instance/asdaxllkmda64"
-            aws_id = "string"
-            aws_region = "eu-west-2"
-            use_instance_credentials = true
-            retention_days = 3
-        }
 
-        tcp_clients {
-            name = "Company SIEM"
-            host = "siem.company.com"
-            port = 8888
-            format = "json"
-            use_tls = true
+    nics {
+      enabled = true
+      name    = "eth1"
+      ipv4 {
+        dhcp {
+          enabled = true
+          dns     = false
+          routers = false
+          ntp     = false
         }
-        sites = [
-            data.appgate_site.default_site.id
-        ]
+      }
     }
+
+    dns_servers = [
+      "8.8.8.8",
+      "1.1.1.1",
+    ]
+    dns_domains = [
+      "aa.com"
+    ]
+    routes {
+      address = "0.0.0.0"
+      netmask = 24
+      gateway = "1.2.3.4"
+      nic     = "eth0"
+    }
+  }
+  controller {
+    enabled = true
+  }
+  snmp_server {
+    enabled    = true
+    tcp_port   = 161
+    udp_port   = 161
+    snmpd_conf = "foo"
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
+    }
+  }
+  healthcheck_server {
+    enabled = true
+    port    = 5555
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
+    }
+  }
+  prometheus_exporter {
+    enabled = true
+    port    = 1234
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
+    }
+  }
+  ping {
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
+    }
+  }
+  log_forwarder {
+    enabled = true
+    elasticsearch {
+      url                      = "https://aws.com/elasticsearch/instance/asdaxllkmda64"
+      aws_id                   = "string"
+      aws_region               = "eu-west-2"
+      use_instance_credentials = true
+      retention_days           = 3
+    }
+
+    tcp_clients {
+      name    = "Company SIEM"
+      host    = "siem.company.com"
+      port    = 8888
+      format  = "json"
+      use_tls = true
+    }
+    sites = [
+      data.appgate_site.default_site.id
+    ]
+  }
 }
 `)
 }
 func testAccCheckApplianceBasicControllerUpdate() string {
 	return fmt.Sprintf(`
 data "appgate_site" "default_site" {
-	   site_name = "Default site"
+  site_name = "Default site"
 }
 
 resource "appgate_appliance" "test_controller" {
-	name     = "controller-test"
-	hostname = "envy-10-97-168-1337.devops"
+  name     = "controller-test"
+  hostname = "envy-10-97-168-1337.devops"
 
-	client_interface {
-        hostname = "envy-10-97-168-1337.devops"
-        proxy_protocol = true
-        https_port     = 444
-        dtls_port      = 445
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-        override_spa_mode = "TCP"
-	}
-
-	peer_interface {
-		hostname   = "envy-10-97-168-1337.devops"
-		https_port = "1337"
-	}
-	tags = [
-		"terraform",
-		"api-test-created"
-	]
-	networking {
-
-		hosts {
-		  hostname = "bla"
-		  address  = "0.0.0.0"
-		}
-
-		nics {
-		  enabled = true
-		  name    = "eth0"
-		  ipv4 {
-			dhcp {
-			  enabled = true
-			  dns     = true
-			  routers = true
-			  ntp     = true
-			}
-		  }
-		}
-	}
-	controller {
-		enabled = true
+  client_interface {
+    hostname       = "envy-10-97-168-1337.devops"
+    proxy_protocol = true
+    https_port     = 444
+    dtls_port      = 445
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
     }
+    override_spa_mode = "TCP"
+  }
+
+  peer_interface {
+    hostname   = "envy-10-97-168-1337.devops"
+    https_port = "1337"
+  }
+  tags = [
+    "terraform",
+    "api-test-created"
+  ]
+  networking {
+
+    hosts {
+      hostname = "bla"
+      address  = "0.0.0.0"
+    }
+
+    nics {
+      enabled = true
+      name    = "eth0"
+      ipv4 {
+        dhcp {
+          enabled = true
+          dns     = true
+          routers = true
+          ntp     = true
+        }
+      }
+    }
+  }
+  controller {
+    enabled = true
+  }
 }
 `)
 }
@@ -471,91 +496,91 @@ func TestAccApplianceIoTConnector(t *testing.T) {
 func testAccCheckApplianceBasicIotConnector() string {
 	return fmt.Sprintf(`
 data "appgate_site" "default_site" {
-	   site_name = "Default site"
+  site_name = "Default site"
 }
 
 resource "appgate_appliance" "iot_connector" {
-	name     = "iot-connector-test"
-	hostname = "envy-10-97-168-1234.devops"
+  name     = "iot-connector-test"
+  hostname = "envy-10-97-168-1234.devops"
 
-	client_interface {
-        hostname = "envy-10-97-168-1234.devops"
-        proxy_protocol = true
-        https_port     = 444
-        dtls_port      = 445
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-        override_spa_mode = "TCP"
-	}
+  client_interface {
+    hostname       = "envy-10-97-168-1234.devops"
+    proxy_protocol = true
+    https_port     = 444
+    dtls_port      = 445
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
+    }
+    override_spa_mode = "TCP"
+  }
 
-	peer_interface {
-		hostname   = "envy-10-97-168-1234.devops"
-		https_port = "1337"
-	}
-	tags = [
-		"terraform",
-		"api-test-created"
-	]
-	networking {
+  peer_interface {
+    hostname   = "envy-10-97-168-1234.devops"
+    https_port = "1337"
+  }
+  tags = [
+    "terraform",
+    "api-test-created"
+  ]
+  networking {
 
-		hosts {
-		  hostname = "bla"
-		  address  = "0.0.0.0"
-		}
-
-		nics {
-		  enabled = true
-		  name    = "eth0"
-		  ipv4 {
-			dhcp {
-			  enabled = true
-			  dns     = true
-			  routers = true
-			  ntp     = true
-			}
-		  }
-		}
-		dns_servers = [
-		  "8.8.8.8",
-		  "1.1.1.1",
-		]
-		dns_domains = [
-		  "aa.com"
-        ]
-        routes {
-            address = "0.0.0.0"
-            netmask = 24
-            gateway = "1.2.3.4"
-            nic = "eth0"
-        }
-	}
-    iot_connector {
-        enabled = true
-        clients {
-          name      = "Printers"
-          device_id = "12699e27-b584-464a-81ee-5b4784b6d425"
-
-          sources {
-            address = "1.3.3.7"
-            netmask = 24
-            nic     = "eth0"
-          }
-          snat = true
-        }
+    hosts {
+      hostname = "bla"
+      address  = "0.0.0.0"
     }
 
-    rsyslog_destinations {
-        selector    = "*.*"
-        template    = "hostname"
-        destination = "10.10.10.2"
+    nics {
+      enabled = true
+      name    = "eth0"
+      ipv4 {
+        dhcp {
+          enabled = true
+          dns     = true
+          routers = true
+          ntp     = true
+        }
+      }
     }
-    hostname_aliases = [
-        "appgatealias.company.com",
-        "alias2.appgate.company.com"
+    dns_servers = [
+      "8.8.8.8",
+      "1.1.1.1",
     ]
+    dns_domains = [
+      "aa.com"
+    ]
+    routes {
+      address = "0.0.0.0"
+      netmask = 24
+      gateway = "1.2.3.4"
+      nic     = "eth0"
+    }
+  }
+  iot_connector {
+    enabled = true
+    clients {
+      name      = "Printers"
+      device_id = "12699e27-b584-464a-81ee-5b4784b6d425"
+
+      sources {
+        address = "1.3.3.7"
+        netmask = 24
+        nic     = "eth0"
+      }
+      snat = true
+    }
+  }
+
+  rsyslog_destinations {
+    selector    = "*.*"
+    template    = "hostname"
+    destination = "10.10.10.2"
+  }
+  hostname_aliases = [
+    "appgatealias.company.com",
+    "alias2.appgate.company.com"
+  ]
 }
 `)
 }
@@ -644,64 +669,64 @@ func TestAccApplianceBasicGateway(t *testing.T) {
 func testAccCheckApplianceBasicGateway() string {
 	return fmt.Sprintf(`
 data "appgate_site" "default_site" {
-	   site_name = "Default site"
+  site_name = "Default site"
 }
 
 resource "appgate_appliance" "test_gateway" {
-	name     = "gateway-test"
-	hostname = "envy-10-97-168-1338.devops"
-    site  = data.appgate_site.default_site.id
-	client_interface {
-        hostname = "envy-10-97-168-1338.devops"
-        proxy_protocol = true
-        https_port     = 444
-        dtls_port      = 445
-        allow_sources {
-          address = "1.3.3.7"
-          netmask = 0
-          nic     = "eth0"
-        }
-        override_spa_mode = "TCP"
-	}
-
-	peer_interface {
-		hostname   = "envy-10-97-168-1338.devops"
-		https_port = "1337"
-	}
-	tags = [
-		"terraform",
-		"api-test-created"
-	]
-	networking {
-
-		hosts {
-		  hostname = "bla"
-		  address  = "0.0.0.0"
-		}
-
-		nics {
-		  enabled = true
-		  name    = "eth0"
-		  ipv4 {
-			dhcp {
-			  enabled = true
-			  dns     = true
-			  routers = true
-			  ntp     = true
-			}
-		  }
-		}
-	}
-    gateway {
-        enabled = true
-        vpn {
-          weight = 100
-          allow_destinations {
-            address = "0.0.0.0"
-            nic     = "eth0"
-          }
-        }
+  name     = "gateway-test"
+  hostname = "envy-10-97-168-1338.devops"
+  site     = data.appgate_site.default_site.id
+  client_interface {
+    hostname       = "envy-10-97-168-1338.devops"
+    proxy_protocol = true
+    https_port     = 444
+    dtls_port      = 445
+    allow_sources {
+      address = "1.3.3.7"
+      netmask = 0
+      nic     = "eth0"
     }
+    override_spa_mode = "TCP"
+  }
+
+  peer_interface {
+    hostname   = "envy-10-97-168-1338.devops"
+    https_port = "1337"
+  }
+  tags = [
+    "terraform",
+    "api-test-created"
+  ]
+  networking {
+
+    hosts {
+      hostname = "bla"
+      address  = "0.0.0.0"
+    }
+
+    nics {
+      enabled = true
+      name    = "eth0"
+      ipv4 {
+        dhcp {
+          enabled = true
+          dns     = true
+          routers = true
+          ntp     = true
+        }
+      }
+    }
+  }
+  gateway {
+    enabled = true
+    vpn {
+      weight = 100
+      allow_destinations {
+        address = "0.0.0.0"
+        nic     = "eth0"
+      }
+    }
+  }
 }
 `)
 }
