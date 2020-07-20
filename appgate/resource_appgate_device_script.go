d4636f1b618b323aa3b60ea15eed03d0dfa4d284
Daniel Nilsson <daniel.nilsson@appgate.com>
2020-07-20 16:11:16 +0200

    resource device script.
    new resource, appgate.device_script.
    
    included basic acceptance test.
    
    Signed-off-by: Daniel Nilsson <daniel.nilsson@appgate.com>

---
+++ appgate/resource_appgate_device_script.go
@@ -0,0 +1,229 @@
+package appgate
+
+import (
+	"bufio"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/appgate/terraform-provider-appgate/client/v12/openapi"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceAppgateDeviceScript() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAppgateDeviceScriptCreate,
+		Read:   resourceAppgateDeviceScriptRead,
+		Update: resourceAppgateDeviceScriptUpdate,
+		Delete: resourceAppgateDeviceScriptDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		SchemaVersion: 1,
+		Schema: map[string]*schema.Schema{
+
+			"device_script_id": {
+				Type:        schema.TypeString,
+				Description: "ID of the object.",
+				Computed:    true,
+			},
+
+			"name": {
+				Type:        schema.TypeString,
+				Description: "Name of the object.",
+				Required:    true,
+			},
+
+			"notes": {
+				Type:        schema.TypeString,
+				Description: "Notes for the object. Used for documentation purposes.",
+				Default:     DefaultDescription,
+				Optional:    true,
+			},
+
+			"tags": {
+				Type:        schema.TypeSet,
+				Description: "Array of tags.",
+				Optional:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+
+			"filename": {
+				Type:        schema.TypeString,
+				Description: "The name of the file to be downloaded as to the client devices.",
+				Required:    true,
+			},
+
+			"file": {
+				Type:          schema.TypeString,
+				Description:   "Path to the Device Script binary.",
+				Optional:      true,
+				ConflictsWith: []string{"content"},
+			},
+
+			"content": {
+				Type:          schema.TypeString,
+				Description:   "The Device Script content.",
+				Optional:      true,
+				ConflictsWith: []string{"file"},
+			},
+
+			"checksum_sha256": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAppgateDeviceScriptCreate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Creating Device script: %s", d.Get("name").(string))
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.DeviceScriptsApi
+	args := openapi.NewDeviceScriptWithDefaults()
+	args.Id = uuid.New().String()
+	args.SetName(d.Get("name").(string))
+	args.SetNotes(d.Get("notes").(string))
+	args.SetFilename(d.Get("filename").(string))
+	args.SetTags(schemaExtractTags(d))
+
+	content, err := getDeviceScriptContent(d)
+	if err != nil {
+		return err
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(content)
+	args.SetFile(encoded)
+
+	request := api.DeviceScriptsPost(context.TODO())
+	request = request.DeviceScript(*args)
+
+	deviceScript, _, err := request.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Could not create Device script %+v", prettyPrintAPIError(err))
+	}
+
+	d.SetId(deviceScript.Id)
+	d.Set("device_script_id", deviceScript.Id)
+
+	return resourceAppgateDeviceScriptRead(d, meta)
+}
+
+func getDeviceScriptContent(d *schema.ResourceData) ([]byte, error) {
+	var content []byte
+	if v, ok := d.GetOk("file"); ok {
+		path := v.(string)
+		file, err := os.Open(path)
+		if err != nil {
+			return nil, fmt.Errorf("Error opening device script file (%s): %s", path, err)
+		}
+		defer func() {
+			err := file.Close()
+			if err != nil {
+				log.Printf("[WARN] Error closing device script file (%s): %s", path, err)
+			}
+		}()
+		reader := bufio.NewReader(file)
+		content, err = ioutil.ReadAll(reader)
+		if err != nil {
+			return nil, fmt.Errorf("Error reading device script file (%s): %s", path, err)
+		}
+
+	} else if v, ok := d.GetOk("content"); ok {
+		content = []byte(v.(string))
+	}
+	return content, nil
+}
+
+func resourceAppgateDeviceScriptRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Reading Device script id: %+v", d.Id())
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.DeviceScriptsApi
+	ctx := context.TODO()
+	request := api.DeviceScriptsIdGet(ctx, d.Id())
+	deviceScript, _, err := request.Authorization(token).Execute()
+	if err != nil {
+		// TODO check if 404
+		d.SetId("")
+		return fmt.Errorf("Failed to read Device script, %+v", err)
+	}
+	d.SetId(deviceScript.Id)
+	d.Set("device_script_id", deviceScript.Id)
+	d.Set("name", deviceScript.Name)
+	d.Set("notes", deviceScript.Notes)
+	d.Set("tags", deviceScript.Tags)
+	d.Set("checksum_sha256", deviceScript.GetChecksumSha256())
+
+	return nil
+}
+
+func resourceAppgateDeviceScriptUpdate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Updating Device script: %s", d.Get("name").(string))
+	log.Printf("[DEBUG] Updating Device script id: %+v", d.Id())
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.DeviceScriptsApi
+	ctx := context.TODO()
+	request := api.DeviceScriptsIdGet(ctx, d.Id())
+	originalDeviceScript, _, err := request.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Failed to read Device script while updating, %+v", err)
+	}
+
+	if d.HasChange("name") {
+		originalDeviceScript.SetName(d.Get("name").(string))
+	}
+
+	if d.HasChange("notes") {
+		originalDeviceScript.SetNotes(d.Get("notes").(string))
+	}
+
+	if d.HasChange("tags") {
+		originalDeviceScript.SetTags(schemaExtractTags(d))
+	}
+
+	if d.HasChange("file") || d.HasChange("content") {
+		content, err := getDeviceScriptContent(d)
+		if err != nil {
+			return err
+		}
+
+		encoded := base64.StdEncoding.EncodeToString(content)
+		originalDeviceScript.SetFile(encoded)
+	}
+
+	req := api.DeviceScriptsIdPut(ctx, d.Id())
+	req = req.DeviceScript(originalDeviceScript)
+	_, _, err = req.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Could not update Device script %+v", prettyPrintAPIError(err))
+	}
+	return resourceAppgateDeviceScriptRead(d, meta)
+}
+
+func resourceAppgateDeviceScriptDelete(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Delete Device script: %s", d.Get("name").(string))
+	log.Printf("[DEBUG] Reading Device script id: %+v", d.Id())
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.DeviceScriptsApi
+
+	request := api.DeviceScriptsIdDelete(context.TODO(), d.Id())
+
+	_, err := request.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Could not delete Device script %+v", prettyPrintAPIError(err))
+	}
+	d.SetId("")
+	return nil
+}
---
+++ appgate/resource_appgate_device_script_test.go
@@ -0,0 +1,108 @@
+package appgate
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccDeviceScriptBasic(t *testing.T) {
+	resourceName := "appgate_device_script.test_device_script"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDeviceScriptDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckDeviceScriptBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDeviceScriptExists(resourceName),
+
+					resource.TestCheckResourceAttr(resourceName, "checksum_sha256", "74443048b52bf2be3b0f003a8f37592551d316c6c7c28b1a110ee6f879ef4130"),
+					resource.TestCheckResourceAttr(resourceName, "content", "#!/usr/bin/env bash\necho \"hello world\"\n"),
+
+					resource.TestCheckResourceAttr(resourceName, "filename", "acceptance_script.sh"),
+					resource.TestCheckResourceAttr(resourceName, "name", "device_script_one"),
+					resource.TestCheckResourceAttr(resourceName, "notes", "Managed by terraform"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.2876187004", "api-created"),
+					resource.TestCheckResourceAttr(resourceName, "tags.535570215", "terraform"),
+				),
+			},
+			{
+				ResourceName:     resourceName,
+				ImportState:      true,
+				ImportStateCheck: testAccDeviceScriptImportStateCheckFunc(1),
+			},
+		},
+	})
+}
+
+func testAccCheckDeviceScriptBasic() string {
+	return fmt.Sprintf(`
+resource "appgate_device_script" "test_device_script" {
+  name     = "device_script_one"
+  filename = "acceptance_script.sh"
+  content  = <<-EOF
+#!/usr/bin/env bash
+echo "hello world"
+EOF
+  tags = [
+    "terraform",
+    "api-created"
+  ]
+}
+`)
+}
+
+func testAccCheckDeviceScriptExists(resource string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		token := testAccProvider.Meta().(*Client).Token
+		api := testAccProvider.Meta().(*Client).API.DeviceScriptsApi
+
+		rs, ok := state.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resource)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+
+		_, _, err := api.DeviceScriptsIdGet(context.Background(), rs.Primary.ID).Authorization(token).Execute()
+		if err != nil {
+			return fmt.Errorf("error fetching device script with resource %s. %s", resource, err)
+		}
+		return nil
+	}
+}
+
+func testAccCheckDeviceScriptDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "appgate_device_script" {
+			continue
+		}
+
+		token := testAccProvider.Meta().(*Client).Token
+		api := testAccProvider.Meta().(*Client).API.DeviceScriptsApi
+
+		_, _, err := api.DeviceScriptsIdGet(context.Background(), rs.Primary.ID).Authorization(token).Execute()
+		if err == nil {
+			return fmt.Errorf("Device script still exists, %+v", err)
+		}
+	}
+	return nil
+}
+
+func testAccDeviceScriptImportStateCheckFunc(expectedStates int) resource.ImportStateCheckFunc {
+	return func(s []*terraform.InstanceState) error {
+		if len(s) != expectedStates {
+			return fmt.Errorf("expected %d states, got %d: %+v", expectedStates, len(s), s)
+		}
+		return nil
+	}
+}
