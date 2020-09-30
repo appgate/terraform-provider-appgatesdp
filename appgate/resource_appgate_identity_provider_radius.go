7c472d3c02bb3393e88ccd9a13f1fdc702509577
Daniel Nilsson <daniel.nilsson@appgate.com>
2020-09-30 09:21:20 +0200

    radius identity provider.
    Signed-off-by: Daniel Nilsson <daniel.nilsson@appgate.com>

---
+++ appgate/resource_appgate_identity_provider_radius.go
@@ -0,0 +1,328 @@
+package appgate
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/appgate/terraform-provider-appgate/client/v12/openapi"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAppgateRadiusProvider() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAppgateRadiusProviderRuleCreate,
+		Read:   resourceAppgateRadiusProviderRuleRead,
+		Update: resourceAppgateRadiusProviderRuleUpdate,
+		Delete: identityProviderDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: func() map[string]*schema.Schema {
+			s := identityProviderSchema()
+			s["type"].Default = identityProviderRadius
+
+			s["hostnames"] = &schema.Schema{
+				Type:     schema.TypeList,
+				Required: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			}
+			s["port"] = &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			}
+			s["shared_secret"] = &schema.Schema{
+				Type:      schema.TypeString,
+				Required:  true,
+				Sensitive: true,
+			}
+			s["authentication_protocol"] = &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "CHAP",
+				ValidateFunc: func(v interface{}, name string) (warns []string, errs []error) {
+					s := v.(string)
+					list := []string{"PAP", "CHAP"}
+					for _, x := range list {
+						if s == x {
+							return
+						}
+					}
+					errs = append(errs, fmt.Errorf("authentication_protocol must be on of %v, got %s", list, s))
+					return
+				},
+			}
+
+			return s
+		}(),
+	}
+}
+
+func resourceAppgateRadiusProviderRuleCreate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Creating RadiusProvider: %s", d.Get("name").(string))
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.RadiusIdentityProvidersApi
+	ctx := context.TODO()
+	provider := openapi.NewIdentityProvider(identityProviderRadius)
+	provider, err := readProviderFromConfig(d, *provider)
+	if err != nil {
+		return fmt.Errorf("Failed to read and create basic identity provider for %s %s", identityProviderRadius, err)
+	}
+	args := openapi.NewRadiusProviderWithDefaults()
+	// base
+	args.SetType(provider.GetType())
+	args.SetId(provider.GetId())
+	args.SetName(provider.GetName())
+	args.SetNotes(provider.GetNotes())
+	args.SetTags(provider.GetTags())
+	// identity provider
+	if provider.Default != nil {
+		args.SetDefault(provider.GetDefault())
+	}
+	if provider.ClientProvider != nil {
+		args.SetClientProvider(*provider.ClientProvider)
+	}
+	if provider.AdminProvider != nil {
+		args.SetAdminProvider(*provider.AdminProvider)
+	}
+	if provider.OnBoarding2FA != nil {
+		args.SetOnBoarding2FA(*provider.OnBoarding2FA)
+	}
+	if provider.OnBoardingType != nil {
+		args.SetOnBoardingType(*provider.OnBoardingType)
+	}
+	if provider.OnBoardingOtpProvider != nil {
+		args.SetOnBoardingOtpProvider(*provider.OnBoardingOtpProvider)
+	}
+	if provider.OnBoardingOtpMessage != nil {
+		args.SetOnBoardingOtpMessage(*provider.OnBoardingOtpMessage)
+	}
+	if provider.InactivityTimeoutMinutes != nil {
+		args.SetInactivityTimeoutMinutes(*provider.InactivityTimeoutMinutes)
+	}
+	if provider.IpPoolV4 != nil {
+		args.SetIpPoolV4(*provider.IpPoolV4)
+	}
+	if provider.IpPoolV6 != nil {
+		args.SetIpPoolV6(*provider.IpPoolV6)
+	}
+	if provider.DnsServers != nil {
+		args.SetDnsServers(*provider.DnsServers)
+	}
+	if provider.DnsSearchDomains != nil {
+		args.SetDnsSearchDomains(*provider.DnsSearchDomains)
+	}
+	if provider.BlockLocalDnsRequests != nil {
+		args.SetBlockLocalDnsRequests(*provider.BlockLocalDnsRequests)
+	}
+	if provider.ClaimMappings != nil {
+		args.SetClaimMappings(*provider.ClaimMappings)
+	}
+	if provider.OnDemandClaimMappings != nil {
+		args.SetOnDemandClaimMappings(*provider.OnDemandClaimMappings)
+	}
+	// radius
+	if v, ok := d.GetOk("hostnames"); ok {
+		hostnames, err := readArrayOfStringsFromConfig(v.([]interface{}))
+		if err != nil {
+			return err
+		}
+		args.SetHostnames(hostnames)
+	}
+	if v, ok := d.GetOk("port"); ok {
+		args.SetPort(int32(v.(int)))
+	}
+	if v, ok := d.GetOk("shared_secret"); ok {
+		args.SetSharedSecret(v.(string))
+	}
+	if v, ok := d.GetOk("authentication_protocol"); ok {
+		args.SetAuthenticationProtocol(v.(string))
+	}
+
+	request := api.IdentityProvidersPost(ctx)
+	p, _, err := request.IdentityProvider(*args).Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Could not create %s provider %+v", identityProviderRadius, prettyPrintAPIError(err))
+	}
+	d.SetId(p.Id)
+	return resourceAppgateRadiusProviderRuleRead(d, meta)
+}
+
+func resourceAppgateRadiusProviderRuleRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Reading radius identity provider id: %+v", d.Id())
+
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.RadiusIdentityProvidersApi
+	ctx := context.TODO()
+	request := api.IdentityProvidersIdGet(ctx, d.Id())
+	radius, _, err := request.Authorization(token).Execute()
+	if err != nil {
+		d.SetId("")
+		return fmt.Errorf("Failed to read LDAP Identity provider, %+v", err)
+	}
+	d.Set("type", identityProviderRadius)
+	// base attributes
+	d.Set("name", radius.Name)
+	d.Set("notes", radius.Notes)
+	d.Set("tags", radius.Tags)
+
+	// identity provider attributes
+	d.Set("default", radius.GetDefault())
+	d.Set("client_provider", radius.GetClientProvider())
+	d.Set("admin_provider", radius.GetAdminProvider())
+	if v, ok := radius.GetOnBoarding2FAOk(); ok {
+		if err := d.Set("on_boarding_two_factor", flattenIdentityProviderOnboarding2fa(*v)); err != nil {
+			return err
+		}
+	}
+
+	d.Set("inactivity_timeout_minutes", radius.GetInactivityTimeoutMinutes())
+	if v, ok := radius.GetIpPoolV4Ok(); ok {
+		d.Set("ip_pool_v4", *v)
+	}
+	if v, ok := radius.GetIpPoolV6Ok(); ok {
+		d.Set("ip_pool_v6", *v)
+	}
+
+	d.Set("dns_servers", radius.GetDnsServers())
+	d.Set("dns_search_domains", radius.GetDnsSearchDomains())
+	d.Set("block_local_dns_requests", radius.GetBlockLocalDnsRequests())
+	if v, ok := radius.GetClaimMappingsOk(); ok {
+		if err := d.Set("claim_mappings", flattenIdentityProviderClaimsMappning(*v)); err != nil {
+			return err
+		}
+	}
+
+	if v, ok := radius.GetOnDemandClaimMappingsOk(); ok {
+		if err := d.Set("on_demand_claim_mappings", flattenIdentityProviderOnDemandClaimsMappning(*v)); err != nil {
+			return err
+		}
+	}
+
+	// radius attributes
+	if v, ok := radius.GetHostnamesOk(); ok {
+		d.Set("hostnames", *v)
+	}
+	if v, ok := radius.GetPortOk(); ok {
+		d.Set("port", *v)
+	}
+	if val, ok := d.GetOk("shared_secret"); ok {
+		d.Set("shared_secret", val)
+	}
+	if v, ok := radius.GetAuthenticationProtocolOk(); ok {
+		d.Set("authentication_protocol", *v)
+	}
+	return nil
+}
+
+func resourceAppgateRadiusProviderRuleUpdate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Updating radius identity provider id: %+v", d.Id())
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.RadiusIdentityProvidersApi
+	ctx := context.TODO()
+	request := api.IdentityProvidersIdGet(ctx, d.Id())
+	originalRadiusProvider, _, err := request.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Failed to read LDAP Identity provider, %+v", err)
+	}
+	// base attributes
+	if d.HasChange("name") {
+		originalRadiusProvider.SetName(d.Get("name").(string))
+	}
+
+	if d.HasChange("notes") {
+		originalRadiusProvider.SetNotes(d.Get("notes").(string))
+	}
+
+	if d.HasChange("tags") {
+		originalRadiusProvider.SetTags(schemaExtractTags(d))
+	}
+
+	// identity provider attributes
+	if d.HasChange("default") {
+		originalRadiusProvider.SetDefault(d.Get("default").(bool))
+	}
+	if d.HasChange("client_provider") {
+		originalRadiusProvider.SetClientProvider(d.Get("client_provider").(bool))
+	}
+	if d.HasChange("admin_provider") {
+		originalRadiusProvider.SetAdminProvider(d.Get("admin_provider").(bool))
+	}
+	if d.HasChange("on_boarding_two_factor") {
+		_, v := d.GetChange("on_boarding_two_factor")
+		onboarding := readOnBoardingTwoFactorFromConfig(v.([]interface{}))
+		originalRadiusProvider.SetOnBoarding2FA(onboarding)
+	}
+
+	if d.HasChange("inactivity_timeout_minutes") {
+		originalRadiusProvider.SetInactivityTimeoutMinutes(int32(d.Get("inactivity_timeout_minutes").(int)))
+	}
+	if d.HasChange("ip_pool_v4") {
+		originalRadiusProvider.SetIpPoolV4(d.Get("ip_pool_v4").(string))
+	}
+	if d.HasChange("ip_pool_v6") {
+		originalRadiusProvider.SetIpPoolV6(d.Get("ip_pool_v6").(string))
+	}
+	if d.HasChange("dns_servers") {
+		_, v := d.GetChange("dns_servers")
+		servers, err := readArrayOfStringsFromConfig(v.([]interface{}))
+		if err != nil {
+			return fmt.Errorf("Failed to read dns servers %s", err)
+		}
+		originalRadiusProvider.SetDnsServers(servers)
+	}
+	if d.HasChange("dns_search_domains") {
+		_, v := d.GetChange("dns_search_domains")
+		servers, err := readArrayOfStringsFromConfig(v.([]interface{}))
+		if err != nil {
+			return fmt.Errorf("Failed to read dns search domains %s", err)
+		}
+		originalRadiusProvider.SetDnsSearchDomains(servers)
+	}
+	if d.HasChange("block_local_dns_requests") {
+		originalRadiusProvider.SetBlockLocalDnsRequests(d.Get("block_local_dns_requests").(bool))
+	}
+	if d.HasChange("claim_mappings") {
+		_, v := d.GetChange("claim_mappings")
+		claims := readIdentityProviderClaimMappingFromConfig(v.([]interface{}))
+		originalRadiusProvider.SetClaimMappings(claims)
+	}
+	if d.HasChange("on_demand_claim_mappings") {
+		_, v := d.GetChange("on_demand_claim_mappings")
+		claims := readIdentityProviderOnDemandClaimMappingFromConfig(v.([]interface{}))
+		originalRadiusProvider.SetOnDemandClaimMappings(claims)
+	}
+
+	// radius provider attributes
+	if d.HasChange("hostnames") {
+		_, v := d.GetChange("hostnames")
+		servers, err := readArrayOfStringsFromConfig(v.([]interface{}))
+		if err != nil {
+			return fmt.Errorf("Failed to read hostnames %s", err)
+		}
+		originalRadiusProvider.SetHostnames(servers)
+	}
+	if d.HasChange("port") {
+		_, v := d.GetChange("port")
+		originalRadiusProvider.SetPort(int32(v.(int)))
+	}
+
+	originalRadiusProvider.SetSharedSecret(d.Get("shared_secret").(string))
+
+	req := api.IdentityProvidersIdPut(ctx, d.Id())
+	req = req.IdentityProvider(originalRadiusProvider)
+	_, _, err = req.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Could not update %s provider %+v", identityProviderRadius, prettyPrintAPIError(err))
+	}
+	return resourceAppgateRadiusProviderRuleRead(d, meta)
+}
---
+++ appgate/resource_appgate_identity_provider_radius_test.go
@@ -0,0 +1,228 @@
+package appgate
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccRadiusIdentityProviderBasic(t *testing.T) {
+	resourceName := "appgate_radius_identity_provider.radius_test_resource"
+	rName := RandStringFromCharSet(10, CharSetAlphaNum)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRadiusIdentityProviderDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckRadiusIdentityProviderBasic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckRadiusIdentityProviderExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "admin_provider", "true"),
+					resource.TestCheckResourceAttr(resourceName, "authentication_protocol", "CHAP"),
+					resource.TestCheckResourceAttr(resourceName, "block_local_dns_requests", "true"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.#", "6"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.attribute_name", "objectGUID"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.claim_name", "userId"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.attribute_name", "sAMAccountName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.claim_name", "username"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.attribute_name", "givenName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.claim_name", "firstName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.attribute_name", "sn"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.claim_name", "lastName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.attribute_name", "mail"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.claim_name", "emails"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.list", "true"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.attribute_name", "memberOf"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.claim_name", "groups"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.list", "true"),
+					resource.TestCheckResourceAttr(resourceName, "default", "false"),
+					resource.TestCheckResourceAttr(resourceName, "dns_search_domains.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "dns_search_domains.0", "internal.company.com"),
+					resource.TestCheckResourceAttr(resourceName, "dns_servers.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "dns_servers.0", "172.17.18.19"),
+					resource.TestCheckResourceAttr(resourceName, "dns_servers.1", "192.100.111.31"),
+					resource.TestCheckResourceAttr(resourceName, "hostnames.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "hostnames.0", "radius.company.com"),
+					resource.TestCheckResourceAttr(resourceName, "inactivity_timeout_minutes", "0"),
+					resource.TestCheckResourceAttr(resourceName, "ip_pool_v4", "f572b4ab-7963-4a90-9e5a-3bf033bfe2cc"),
+					resource.TestCheckResourceAttr(resourceName, "ip_pool_v6", "6935b379-205d-4fdd-847f-a0b5f14aff53"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "notes", "Managed by terraform"),
+					resource.TestCheckResourceAttr(resourceName, "on_boarding_two_factor.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "on_boarding_two_factor.0.device_limit_per_user", "6"),
+					resource.TestCheckResourceAttr(resourceName, "on_boarding_two_factor.0.message", "welcome"),
+					resource.TestCheckResourceAttr(resourceName, "on_boarding_two_factor.0.mfa_provider_id", "3ae98d53-c520-437f-99e4-451f936e6d2c"),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.0.claim_name", "antiVirusIsRunning"),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.0.command", "fileSize"),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.0.parameters.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.0.parameters.0.args", ""),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.0.parameters.0.name", ""),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.0.parameters.0.path", "/usr/bin/python3"),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.0.platform", "desktop.windows.all"),
+					resource.TestCheckResourceAttr(resourceName, "port", "1812"),
+					resource.TestCheckResourceAttr(resourceName, "shared_secret", "hunter2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.2876187004", "api-created"),
+					resource.TestCheckResourceAttr(resourceName, "tags.535570215", "terraform"),
+					resource.TestCheckResourceAttr(resourceName, "type", "Radius"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateCheck:        testAccRadiusIdentityProviderImportStateCheckFunc(1),
+				ImportStateVerifyIgnore: []string{"shared_secret"},
+			},
+		},
+	})
+}
+
+func testAccCheckRadiusIdentityProviderBasic(rName string) string {
+	return fmt.Sprintf(`
+data "appgate_ip_pool" "ip_sex_pool" {
+  ip_pool_name = "default pool v6"
+}
+
+data "appgate_ip_pool" "ip_four_pool" {
+  ip_pool_name = "default pool v4"
+}
+
+resource "appgate_radius_identity_provider" "radius_test_resource" {
+  name = "%s"
+  hostnames = [
+    "radius.company.com"
+  ]
+  admin_provider = true
+  port           = 1812
+  shared_secret  = "hunter2"
+  ip_pool_v4     = data.appgate_ip_pool.ip_four_pool.id
+  ip_pool_v6     = data.appgate_ip_pool.ip_sex_pool.id
+  dns_servers = [
+    "172.17.18.19",
+    "192.100.111.31"
+  ]
+  dns_search_domains = [
+    "internal.company.com"
+  ]
+  block_local_dns_requests = true
+  on_boarding_two_factor {
+    mfa_provider_id       = "3ae98d53-c520-437f-99e4-451f936e6d2c"
+    device_limit_per_user = 6
+    message               = "welcome"
+  }
+  tags = [
+    "terraform",
+    "api-created"
+  ]
+  claim_mappings {
+    attribute_name = "objectGUID"
+    claim_name     = "userId"
+    encrypted      = false
+    list           = false
+  }
+  claim_mappings {
+    attribute_name = "sAMAccountName"
+    claim_name     = "username"
+    encrypted      = false
+    list           = false
+  }
+  claim_mappings {
+    attribute_name = "givenName"
+    claim_name     = "firstName"
+    encrypted      = false
+    list           = false
+  }
+  claim_mappings {
+    attribute_name = "sn"
+    claim_name     = "lastName"
+    encrypted      = false
+    list           = false
+  }
+  claim_mappings {
+    attribute_name = "mail"
+    claim_name     = "emails"
+    encrypted      = false
+    list           = true
+  }
+  claim_mappings {
+    attribute_name = "memberOf"
+    claim_name     = "groups"
+    encrypted      = false
+    list           = true
+  }
+
+  on_demand_claim_mappings {
+    command    = "fileSize"
+    claim_name = "antiVirusIsRunning"
+    parameters {
+      path = "/usr/bin/python3"
+    }
+    platform = "desktop.windows.all"
+  }
+}
+`, rName)
+}
+
+func testAccCheckRadiusIdentityProviderExists(resource string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		token := testAccProvider.Meta().(*Client).Token
+		api := testAccProvider.Meta().(*Client).API.RadiusIdentityProvidersApi
+
+		rs, ok := state.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resource)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+
+		_, _, err := api.IdentityProvidersIdGet(context.Background(), rs.Primary.ID).Authorization(token).Execute()
+		if err != nil {
+			return fmt.Errorf("error fetching radius identity provider with resource %s. %s", resource, err)
+		}
+		return nil
+	}
+}
+
+func testAccCheckRadiusIdentityProviderDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "appgate_radius_identity_provider" {
+			continue
+		}
+
+		token := testAccProvider.Meta().(*Client).Token
+		api := testAccProvider.Meta().(*Client).API.RadiusIdentityProvidersApi
+
+		_, _, err := api.IdentityProvidersIdGet(context.Background(), rs.Primary.ID).Authorization(token).Execute()
+		if err == nil {
+			return fmt.Errorf("radius identity provider still exists, %+v", err)
+		}
+	}
+	return nil
+}
+
+func testAccRadiusIdentityProviderImportStateCheckFunc(expectedStates int) resource.ImportStateCheckFunc {
+	return func(s []*terraform.InstanceState) error {
+		if len(s) != expectedStates {
+			return fmt.Errorf("expected %d states, got %d: %+v", expectedStates, len(s), s)
+		}
+		return nil
+	}
+}
