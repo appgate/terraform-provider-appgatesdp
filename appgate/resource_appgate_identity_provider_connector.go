1a2f10dbf46f423151b4b82b9e8494955ab03b98
Daniel Nilsson <daniel.nilsson@appgate.com>
2020-10-21 17:30:11 +0200

    resource identity provider connector.
    Signed-off-by: Daniel Nilsson <daniel.nilsson@appgate.com>

---
+++ appgate/resource_appgate_identity_provider_connector.go
@@ -0,0 +1,168 @@
+package appgate
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/appgate/terraform-provider-appgate/client/v12/openapi"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAppgateConnectorProvider() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAppgateConnectorProviderRuleCreate,
+		Read:   resourceAppgateConnectorProviderRuleRead,
+		Update: resourceAppgateConnectorProviderRuleUpdate,
+		Delete: resourceAppgateConnectorProviderRuleDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: func() map[string]*schema.Schema {
+			s := mergeSchemaMaps(baseEntitySchema(), identityProviderIPPoolSchema(), identityProviderClaimsSchema())
+			s["name"] = &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  builtinProviderConnector,
+			}
+			s["type"] = &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  identityProviderConnector,
+			}
+
+			return s
+		}(),
+	}
+}
+
+func resourceAppgateConnectorProviderRuleDelete(d *schema.ResourceData, meta interface{}) error {
+	// We can't delete the builtin connector identity provider, but we can remove it from the terraform state file.
+	d.SetId("")
+	return nil
+}
+
+func resourceAppgateConnectorProviderRuleCreate(d *schema.ResourceData, meta interface{}) error {
+	// we aren'ẗ allowed to create new additional local identity providers, but we can update existing
+	// with terraform import.
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.ConnectorIdentityProvidersApi
+	ctx := context.TODO()
+	connectorIP, err := getBuiltinConnectorProviderUUID(ctx, *api, token)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(connectorIP.GetId())
+
+	return resourceAppgateConnectorProviderRuleUpdate(d, meta)
+}
+
+func getBuiltinConnectorProviderUUID(ctx context.Context, api openapi.ConnectorIdentityProvidersApiService, token string) (*openapi.IoTConnectorProvider, error) {
+	var connectorIP *openapi.IoTConnectorProvider
+	request := api.IdentityProvidersGet(ctx)
+
+	provider, _, err := request.Query(builtinProviderConnector).OrderBy("name").Range_("0-1").Authorization(token).Execute()
+	if err != nil {
+		return connectorIP, err
+	}
+	for _, s := range provider.GetData() {
+		if s.GetName() == builtinProviderConnector {
+			connectorIP = &s
+		}
+	}
+	if connectorIP == nil {
+		return connectorIP, fmt.Errorf("Could not find bultin connector identity provider")
+	}
+	return connectorIP, nil
+}
+
+func resourceAppgateConnectorProviderRuleRead(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Reading connectorIP identity provider")
+
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.ConnectorIdentityProvidersApi
+	ctx := context.TODO()
+	connectorIP, err := getBuiltinConnectorProviderUUID(ctx, *api, token)
+	if err != nil {
+		d.SetId("")
+		return fmt.Errorf("Failed to read Connector Identity provider, %+v", err)
+	}
+	d.SetId(connectorIP.GetId())
+
+	d.Set("type", identityProviderConnector)
+	// base attributes
+	d.Set("name", connectorIP.Name)
+	d.Set("notes", connectorIP.Notes)
+	d.Set("tags", connectorIP.Tags)
+
+	// identity provider attributes
+	if v, ok := connectorIP.GetIpPoolV4Ok(); ok {
+		d.Set("ip_pool_v4", *v)
+	}
+	if v, ok := connectorIP.GetIpPoolV6Ok(); ok {
+		d.Set("ip_pool_v6", v)
+	}
+
+	if v, ok := connectorIP.GetClaimMappingsOk(); ok {
+		if err := d.Set("claim_mappings", flattenIdentityProviderClaimsMappning(*v)); err != nil {
+			return err
+		}
+	}
+	if v, ok := connectorIP.GetOnDemandClaimMappingsOk(); ok {
+		d.Set("on_demand_claim_mappings", flattenIdentityProviderOnDemandClaimsMappning(*v))
+	}
+
+	return nil
+}
+
+func resourceAppgateConnectorProviderRuleUpdate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] Updating connectorIP identity provider id: %+v", d.Id())
+	token := meta.(*Client).Token
+	api := meta.(*Client).API.ConnectorIdentityProvidersApi
+	ctx := context.TODO()
+	request := api.IdentityProvidersIdGet(ctx, d.Id())
+	originalConnectorProvider, _, err := request.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Failed to read Connector Identity provider, %+v", err)
+	}
+	// base attributes
+	if d.HasChange("name") {
+		originalConnectorProvider.SetName(d.Get("name").(string))
+	}
+
+	if d.HasChange("notes") {
+		originalConnectorProvider.SetNotes(d.Get("notes").(string))
+	}
+
+	if d.HasChange("tags") {
+		originalConnectorProvider.SetTags(schemaExtractTags(d))
+	}
+
+	// identity provider attributes
+	if d.HasChange("ip_pool_v4") {
+		originalConnectorProvider.SetIpPoolV4(d.Get("ip_pool_v4").(string))
+	}
+	if d.HasChange("ip_pool_v6") {
+		originalConnectorProvider.SetIpPoolV6(d.Get("ip_pool_v6").(string))
+	}
+	if d.HasChange("claim_mappings") {
+		_, v := d.GetChange("claim_mappings")
+		claims := readIdentityProviderClaimMappingFromConfig(v.([]interface{}))
+		originalConnectorProvider.SetClaimMappings(claims)
+	}
+	if d.HasChange("on_demand_claim_mappings") {
+		_, v := d.GetChange("on_demand_claim_mappings")
+		claims := readIdentityProviderOnDemandClaimMappingFromConfig(v.([]interface{}))
+		originalConnectorProvider.SetOnDemandClaimMappings(claims)
+	}
+
+	req := api.IdentityProvidersIdPut(ctx, d.Id())
+	req = req.IdentityProvider(originalConnectorProvider)
+	_, _, err = req.Authorization(token).Execute()
+	if err != nil {
+		return fmt.Errorf("Could not update %s provider %+v", identityProviderConnector, prettyPrintAPIError(err))
+	}
+	return resourceAppgateConnectorProviderRuleRead(d, meta)
+}
---
+++ appgate/resource_appgate_identity_provider_connector_test.go
@@ -0,0 +1,97 @@
+package appgate
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccConnectorIdentityProviderBasic(t *testing.T) {
+	resourceName := "appgate_connector_identity_provider.connector_test_resource"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckConnectorIdentityProviderBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "type", identityProviderConnector),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.#", "7"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.attribute_name", "id"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.claim_name", "id"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.0.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.attribute_name", "applianceName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.claim_name", "applianceName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.1.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.attribute_name", "clientName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.claim_name", "clientName"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.2.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.attribute_name", "hostname"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.claim_name", "hostname"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.3.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.attribute_name", "peerHostname"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.claim_name", "peerHostname"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.4.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.attribute_name", "tags"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.claim_name", "tags"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.5.list", "true"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.6.attribute_name", "applianceApiVersion"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.6.claim_name", "applianceApiVersion"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.6.encrypted", "false"),
+					resource.TestCheckResourceAttr(resourceName, "claim_mappings.6.list", "false"),
+					resource.TestCheckResourceAttr(resourceName, "name", builtinProviderConnector),
+					resource.TestCheckResourceAttr(resourceName, "notes", "Built-in Identity Provider on local database."),
+					resource.TestCheckResourceAttr(resourceName, "on_demand_claim_mappings.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.3551210388", "builtin"),
+					resource.TestCheckResourceAttrSet(resourceName, "ip_pool_v4"),
+					resource.TestCheckResourceAttrSet(resourceName, "ip_pool_v6"),
+				),
+			},
+			{
+				ResourceName:     resourceName,
+				ImportState:      true,
+				ImportStateCheck: testAccConnectorIdentityProviderImportStateCheckFunc(1),
+			},
+		},
+	})
+}
+
+func testAccCheckConnectorIdentityProviderBasic() string {
+	return fmt.Sprintf(`
+data "appgate_ip_pool" "ip_v6_pool" {
+  ip_pool_name = "default pool v6"
+}
+
+data "appgate_ip_pool" "ip_v4_pool" {
+  ip_pool_name = "default pool v4"
+}
+
+resource "appgate_connector_identity_provider" "connector_test_resource" {
+  notes      = "Built-in Identity Provider on local database."
+  ip_pool_v4 = data.appgate_ip_pool.ip_v4_pool.id
+  ip_pool_v6 = data.appgate_ip_pool.ip_v6_pool.id
+  tags = [
+    "builtin",
+  ]
+}
+`)
+}
+
+func testAccConnectorIdentityProviderImportStateCheckFunc(expectedStates int) resource.ImportStateCheckFunc {
+	return func(s []*terraform.InstanceState) error {
+		if len(s) != expectedStates {
+			return fmt.Errorf("expected %d states, got %d: %+v", expectedStates, len(s), s)
+		}
+		return nil
+	}
+}
