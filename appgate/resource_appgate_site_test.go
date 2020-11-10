5d5ea7258d7190157308843017ac403469585e21
infosec-shinobi <volmeringderek@gmail.com>
2020-11-09 21:26:17 -0500

    detect and test site network updates

--- appgate/resource_appgate_site_test.go
+++ appgate/resource_appgate_site_test.go
@@ -96,6 +96,35 @@ func TestAccSiteBasic(t *testing.T) {
 				ImportState:      true,
 				ImportStateCheck: testAccCriteriaScripImportStateCheckFunc(1),
 			},
+			{
+				Config: testAccCheckSiteUpdate(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSiteExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", "The test site"),
+					resource.TestCheckResourceAttr(resourceName, "network_subnets.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "network_subnets.1525215129", "10.20.0.0/24"),
+					resource.TestCheckResourceAttr(resourceName, "network_subnets.2657174977", "10.0.0.0/16"),
+				),
+			},
+			{
+				ResourceName:     resourceName,
+				ImportState:      true,
+				ImportStateCheck: testAccCriteriaScripImportStateCheckFunc(1),
+			},
+			{
+				Config: testAccCheckSiteNetworkDelete(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSiteExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", "The test site"),
+					resource.TestCheckResourceAttr(resourceName, "network_subnets.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "network_subnets.1525215129", "10.20.0.0/24"),
+				),
+			},
+			{
+				ResourceName:     resourceName,
+				ImportState:      true,
+				ImportStateCheck: testAccCriteriaScripImportStateCheckFunc(1),
+			},
 		},
 	})
 }
@@ -204,6 +233,155 @@ resource "appgate_site" "test_site" {
     }
 }
 `, rName)
+}
+
+func testAccCheckSiteUpdate() string {
+	return fmt.Sprintf(`
+resource "appgate_site" "test_site" {
+    name       = "The test site"
+    short_name = "tst"
+    tags = [
+        "developer",
+        "api-created",
+	    "updated-tag"
+    ]
+    notes = "This object has been created for test purposes."
+
+	network_subnets = [
+        "10.0.0.0/16",
+        "10.20.0.0/24",
+    ]
+
+    default_gateway {
+        enabled_v4       = false
+        enabled_v6       = false
+        excluded_subnets = []
+    }
+
+    name_resolution {
+        dns_resolvers {
+            name            = "DNS Resolver 1"
+            update_interval = 13
+            servers = [
+                "8.8.8.8",
+                "1.1.1.1"
+            ]
+            search_domains = [
+                "hostname.dns",
+                "foo.bar"
+            ]
+        }
+
+        aws_resolvers {
+            name = "AWS Resolver 1"
+            update_interval    = 59
+            use_iam_role       = true
+            access_key_id      = "string1"
+            secret_access_key  = "string2"
+        }
+
+        azure_resolvers {
+            name            = "Azure Resolver 1"
+            update_interval = 30
+            subscription_id = "string1"
+            tenant_id       = "string2"
+            client_id       = "string3"
+            secret          = "string4"
+        }
+
+        esx_resolvers {
+            name            = "ESX Resolver 1"
+            update_interval = 120
+            hostname        = "string1"
+            username        = "string2"
+            password        = "secret_password"
+        }
+
+        gcp_resolvers {
+            name            = "GCP Resolver 1"
+            update_interval = 360
+            project_filter  = "string1"
+            instance_filter = "string2"
+        }
+    }
+}
+`)
+}
+
+func testAccCheckSiteNetworkDelete() string {
+	return fmt.Sprintf(`
+
+resource "appgate_site" "test_site" {
+    name       = "The test site"
+    short_name = "tst"
+    tags = [
+        "developer",
+        "api-created",
+    	"updated-tag"
+    ]
+	
+	notes = "This object has been created for test purposes."
+    network_subnets = [
+        "10.20.0.0/24"
+    ]
+
+    default_gateway {
+        enabled_v4       = false
+        enabled_v6       = false
+        excluded_subnets = []
+    }
+
+    name_resolution {
+        dns_resolvers {
+            name            = "DNS Resolver 1"
+            update_interval = 13
+            servers = [
+                "8.8.8.8",
+                "1.1.1.1"
+            ]
+            search_domains = [
+                "hostname.dns",
+                "foo.bar"
+            ]
+        }
+
+        aws_resolvers {
+            name = "AWS Resolver 1"
+            update_interval    = 59
+            vpcs               = ["test"]
+            vpc_auto_discovery = true
+            use_iam_role       = true
+            access_key_id      = "string1"
+            secret_access_key  = "string2"
+            resolve_with_master_credentials = true
+        }
+
+        azure_resolvers {
+            name            = "Azure Resolver 1"
+            update_interval = 30
+            subscription_id = "string1"
+            tenant_id       = "string2"
+            client_id       = "string3"
+            secret          = "string4"
+        }
+
+        esx_resolvers {
+            name            = "ESX Resolver 1"
+            update_interval = 120
+            hostname        = "string1"
+            username        = "string2"
+            password        = "secret_password"
+        }
+
+        gcp_resolvers {
+            name            = "GCP Resolver 1"
+            update_interval = 360
+            project_filter  = "string1"
+            instance_filter = "string2"
+        }
+    }
+}
+`)
 }
 
 func testAccCheckSiteExists(resource string) resource.TestCheckFunc {
