29fceaf0418d5308883a1b14a862ef1370882533
Daniel Nilsson <daniel.nilsson@appgate.com>
2020-12-21 16:41:27 +0100

    Support for Debuggable Provider Binaries
    https://www.terraform.io/docs/extend/guides/v2-upgrade-guide.html#support-for-debuggable-provider-binaries

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/appgate/terraform-provider-appgate/appgate"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: appgate.Provider})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/appgate/provider",
+			&plugin.ServeOpts{
+				ProviderFunc: appgate.Provider,
+			})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: appgate.Provider})
+	}
 }
