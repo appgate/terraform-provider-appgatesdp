05468dc9ceef1e93153950f22bbbb02cf6fe77b0
Daniel Nilsson <daniel.nilsson@appgate.com>
2020-12-21 16:42:01 +0100

    setup for acceptance test factories
    https://www.terraform.io/docs/extend/guides/v2-upgrade-guide.html#a-new-acceptance-testing-driver

--- appgate/provider_test.go
+++ appgate/provider_test.go
@@ -1,11 +1,13 @@
 package appgate
 
 import (
+	"context"
 	"math/rand"
 	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 const (
@@ -21,10 +23,24 @@ const (
 var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
 
+// testAccProviderFactories is a static map containing only the main provider instance
+//
+// Use other testAccProviderFactories functions, such as testAccProviderFactoriesAlternate,
+// for tests requiring special provider configurations.
+var testAccProviderFactories map[string]func() (*schema.Provider, error)
+
 func init() {
 	testAccProvider = Provider()
 	testAccProviders = map[string]*schema.Provider{
 		"appgate": testAccProvider,
+	}
+
+	// Always allocate a new provider instance each invocation, otherwise gRPC
+	// ProviderConfigure() can overwrite configuration during concurrent testing.
+	testAccProviderFactories = map[string]func() (*schema.Provider, error){
+		"appgate": func() (*schema.Provider, error) {
+			return Provider(), nil
+		},
 	}
 }
 
@@ -44,6 +60,10 @@ func TestProvider(t *testing.T) {
 	}
 }
 
+func TestProvider_impl(t *testing.T) {
+	var _ *schema.Provider = Provider()
+}
+
 func testAccPreCheck(t *testing.T) {
 	if v := os.Getenv("APPGATE_ADDRESS"); v == "" {
 		t.Fatal("APPGATE_ADDRESS must be set for acceptance tests")
@@ -53,5 +73,9 @@ func testAccPreCheck(t *testing.T) {
 	}
 	if v := os.Getenv("APPGATE_PASSWORD"); v == "" {
 		t.Fatal("APPGATE_PASSWORD must be set for acceptance tests")
+	}
+	err := testAccProvider.Configure(context.Background(), terraform.NewResourceConfigRaw(nil))
+	if err != nil {
+		t.Fatalf("err: %v", err)
 	}
 }
