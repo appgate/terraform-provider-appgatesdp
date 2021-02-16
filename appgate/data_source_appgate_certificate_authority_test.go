61d2dbfb88345c286b4bc6d6edb15bd8caaa87f0
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-02-16 10:44:53 +0100

    new data source, CA Certificate.

---
+++ appgate/data_source_appgate_certificate_authority_test.go
@@ -0,0 +1,32 @@
+package appgate
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccAppgateCADataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: `data "appgate_certificate_authority" "test" {}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "version"),
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "serial"),
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "issuer"),
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "subject"),
+
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "valid_from"),
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "valid_to"),
+
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "fingerprint"),
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "certificate"),
+					resource.TestCheckResourceAttrSet("data.appgate_certificate_authority.test", "subject_public_key"),
+				),
+			},
+		},
+	})
+}
