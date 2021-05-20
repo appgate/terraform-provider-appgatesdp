e7b0f914080e2cea3992a81107aabf0266fad08b
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-05-20 15:44:33 +0200

    Determine appliance version during login.
    in >= 5.4.0 , the version attribute has been removed from the response, so we will guess the appliance version based on the client API header version.

--- appgate/config_test.go
+++ appgate/config_test.go
@@ -10,9 +10,8 @@ import (
 	"testing"
 
 	"github.com/appgate/sdp-api-client-go/api/v15/openapi"
-	"github.com/hashicorp/go-version"
-
 	"github.com/google/uuid"
+	"github.com/hashicorp/go-version"
 )
 
 func TestNewClient(t *testing.T) {
@@ -93,70 +92,146 @@ func TestLoginInternalServerError(t *testing.T) {
 	}
 }
 
-func TestConfig(t *testing.T) {
-	_, _, mux, _, port, teardown := setup()
-	defer teardown()
-	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, `{
-  "version": "4.3.0-20000",
-  "user": {
-    "name": "admin",
-    "needTwoFactorAuth": false,
-    "canAccessAuditLogs": true,
-    "privileges": [
-      {
-        "type": "All",
-        "target": "All",
-        "scope": {
-          "all": true,
-          "ids": [
-            "4c07bc67-57ea-42dd-b702-c2d6c45419fc"
-          ],
-          "tags": [
-            "tag"
-          ]
-        },
-        "defaultTags": [
-          "api-created"
+var (
+	version43Test, _         = version.NewVersion("4.3.0-20000")
+	computed54TestVersion, _ = version.NewVersion("5.4.0+estimated")
+
+	loginResponse54 = `
+{
+    "user": {
+        "name": "admin",
+        "needTwoFactorAuth": false,
+        "canAccessAuditLogs": false,
+        "privileges": [
+            {
+                "type": "All",
+                "target": "All",
+                "scope": {
+                    "all": true,
+                    "ids": [],
+                    "tags": []
+                }
+            }
         ]
-      }
-    ]
-  },
-  "token": "very-long-string",
-  "expires": "2020-01-27T08:50:34Z",
-  "messageOfTheDay": "Welcome to Appgate SDP."
-}`)
-		testMethod(t, r, http.MethodPost)
+    },
+    "token": "very-long-string",
+    "expires": "2021-06-05T06:43:44.101853Z"
+}
+`
 
-	})
-	c := &Config{
-		URL:      fmt.Sprintf("http://localhost:%d", port),
-		Username: "admin",
-		Password: "admin",
-	}
-	appgateClient, err := c.Client()
-	if err != nil {
-		t.Fatalf("Expected appgate client got %v", err)
-	}
+	loginResponsePrior53 = `
+{
+	"version": "4.3.0-20000",
+	"user": {
+		"name": "admin",
+		"needTwoFactorAuth": false,
+		"canAccessAuditLogs": true,
+		"privileges": [
+		{
+			"type": "All",
+			"target": "All",
+			"scope": {
+			"all": true,
+			"ids": [
+				"4c07bc67-57ea-42dd-b702-c2d6c45419fc"
+			],
+			"tags": [
+				"tag"
+			]
+			},
+			"defaultTags": [
+			"api-created"
+			]
+		}
+		]
+	},
+	"token": "very-long-string",
+	"expires": "2020-01-27T08:50:34Z",
+	"messageOfTheDay": "Welcome to Appgate SDP."
+}
+`
+)
 
-	expectedVersion, err := version.NewVersion("4.3.0-20000")
-	if err != nil {
-		t.Fatalf("unable to parse expected version")
-	}
-	if !appgateClient.ApplianceVersion.Equal(expectedVersion) {
-		t.Fatalf("Expected %s, got %s", expectedVersion, appgateClient.ApplianceVersion)
-	}
+func TestConfig(t *testing.T) {
 
-	latestSupportedVersion, err := version.NewVersion(ApplianceVersionMap[DefaultClientVersion])
-	if err != nil {
-		t.Fatalf("unable to parse latest supported version")
+	type fields struct {
+		ResponseBody string
 	}
-	if !appgateClient.LatestSupportedVersion.Equal(latestSupportedVersion) {
-		t.Fatalf("Expected Latest Version%s, got %s", expectedVersion, appgateClient.ApplianceVersion)
+	tests := []struct {
+		name            string
+		fields          fields
+		wantErr         bool
+		expectedVersion *version.Version
+		clientVersion   int
+	}{
+		{
+			name: "test before 5.4",
+			fields: fields{
+				ResponseBody: loginResponsePrior53,
+			},
+			wantErr:         false,
+			expectedVersion: version43Test,
+			clientVersion:   13,
+		},
+		{
+			name: "test 5.4 login",
+			fields: fields{
+				ResponseBody: loginResponse54,
+			},
+			wantErr:         false,
+			expectedVersion: computed54TestVersion,
+			clientVersion:   15,
+		},
+		{
+			name: "invalid client version",
+			fields: fields{
+				ResponseBody: loginResponse54,
+			},
+			wantErr:         true,
+			expectedVersion: computed54TestVersion,
+			clientVersion:   2222,
+		},
 	}
-	if appgateClient.Token != "Bearer very-long-string" {
-		t.Fatalf("Expected token Bearer very-long-string, got %s", appgateClient.Token)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, _, mux, _, port, teardown := setup()
+			defer teardown()
+			mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprint(w, tt.fields.ResponseBody)
+				testMethod(t, r, http.MethodPost)
+
+			})
+			c := &Config{
+				URL:      fmt.Sprintf("http://localhost:%d", port),
+				Username: "admin",
+				Password: "admin",
+				Version:  tt.clientVersion,
+			}
+			appgateClient, err := c.Client()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Config.Client() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if appgateClient == nil && tt.wantErr {
+				return
+			}
+
+			if !appgateClient.ApplianceVersion.Equal(tt.expectedVersion) {
+				t.Fatalf("Expected %s, got %s", tt.expectedVersion, appgateClient.ApplianceVersion)
+			}
+
+			latestSupportedVersion, err := version.NewVersion(ApplianceVersionMap[DefaultClientVersion])
+			if err != nil {
+				t.Fatalf("unable to parse latest supported version")
+			}
+			if !appgateClient.LatestSupportedVersion.Equal(latestSupportedVersion) {
+				t.Fatalf("Expected Latest Version%s, got %s", tt.expectedVersion, appgateClient.ApplianceVersion)
+			}
+			if appgateClient.Token != "Bearer very-long-string" {
+				t.Fatalf("Expected token Bearer very-long-string, got %s", appgateClient.Token)
+			}
+		})
 	}
 }
