e463acf0240f4f942d4d81137e3cda09492031ad
Daniel Nilsson <daniel.nilsson@appgate.com>
2023-05-08 17:09:40 +0200

    acceptance test for #308

--- appgate/resource_appgate_policy_test.go
+++ appgate/resource_appgate_policy_test.go
@@ -22,15 +22,93 @@ func TestAccPolicyBasic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckPolicyExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.%", "10"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.add_remove_profiles", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.attention_level", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.auto_start", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.enabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.entitlements_list", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.keep_me_signed_in", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.quit", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.saml_auto_sign_in", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.sign_out", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.suspend", "Show"),
 					resource.TestCheckResourceAttr(resourceName, "disabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "dns_settings.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "entitlement_links.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "entitlement_links.0", "developer"),
+					resource.TestCheckResourceAttr(resourceName, "entitlements.#", "2"),
+					resource.TestCheckTypeSetElemAttrPair(resourceName, "entitlements.*", "appgatesdp_entitlement.one", "id"),
+					resource.TestCheckTypeSetElemAttrPair(resourceName, "entitlements.*", "appgatesdp_entitlement.two", "id"),
 					resource.TestCheckResourceAttr(resourceName, "expression", "return true;\n"),
 					resource.TestCheckResourceAttr(resourceName, "notes", "terraform policy notes"),
+					resource.TestCheckResourceAttrSet(resourceName, "policy_id"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.%", "3"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.persist", "false"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.url", "http://foo.com"),
+					resource.TestCheckResourceAttr(resourceName, "ringfence_rule_links.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "ringfence_rule_links.0", "developer"),
+					resource.TestCheckResourceAttr(resourceName, "ringfence_rules.#", "0"),
 					resource.TestCheckResourceAttr(resourceName, "tags.#", "2"),
 					resource.TestCheckResourceAttr(resourceName, "tags.0", "api-created"),
 					resource.TestCheckResourceAttr(resourceName, "tags.1", "terraform"),
 					resource.TestCheckResourceAttr(resourceName, "tamper_proofing", "true"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.0.%", "2"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.0.dns_suffix", "aa"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.0.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "type", "Mixed"),
+				),
+			},
+			{
+				ResourceName:     resourceName,
+				ImportState:      true,
+				ImportStateCheck: testAccCriteriaScripImportStateCheckFunc(1),
+			},
+			{
+				Config: testAccCheckPolicyBasicUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPolicyExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "client_profile_settings.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.%", "10"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.add_remove_profiles", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.attention_level", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.auto_start", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.enabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.entitlements_list", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.keep_me_signed_in", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.quit", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.saml_auto_sign_in", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.sign_out", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "client_settings.0.suspend", "Show"),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "dns_settings.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "entitlement_links.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "entitlements.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "expression", "return true;\n"),
+					resource.TestCheckResourceAttr(resourceName, "notes", "terraform policy notes"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.%", "3"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.persist", "false"),
+					resource.TestCheckResourceAttr(resourceName, "proxy_auto_config.0.url", "http://foo.com"),
+					resource.TestCheckResourceAttr(resourceName, "ringfence_rule_links.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "ringfence_rule_links.0", "developer"),
-					resource.TestCheckResourceAttr(resourceName, "entitlement_links.0", "developer"),
+					resource.TestCheckResourceAttr(resourceName, "ringfence_rules.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "tags.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "tags.0", "api-created"),
+					resource.TestCheckResourceAttr(resourceName, "tags.1", "terraform"),
+					resource.TestCheckResourceAttr(resourceName, "tamper_proofing", "true"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.0.%", "2"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.0.dns_suffix", "aa"),
+					resource.TestCheckResourceAttr(resourceName, "trusted_network_check.0.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "type", "Mixed"),
 				),
 			},
 			{
@@ -44,6 +122,56 @@ func TestAccPolicyBasic(t *testing.T) {
 
 func testAccCheckPolicyBasic(rName string) string {
 	return fmt.Sprintf(`
+data "appgatesdp_condition" "always" {
+	condition_name = "Always"
+}
+data "appgatesdp_site" "default_site" {
+	site_name = "Default Site"
+}
+resource "appgatesdp_entitlement" "one" {
+	app_shortcut_scripts = []
+	condition_logic      = "and"
+	conditions = [
+		data.appgatesdp_condition.always.id,
+	]
+	name = "web test one"
+	site = data.appgatesdp_site.default_site.id
+
+	actions {
+		action = "allow"
+		hosts = [
+		"google.se",
+		]
+		ports = [
+		"443",
+		"80",
+		]
+		subtype = "tcp_up"
+
+	}
+}
+resource "appgatesdp_entitlement" "two" {
+	app_shortcut_scripts = []
+	condition_logic      = "and"
+	conditions = [
+		data.appgatesdp_condition.always.id,
+	]
+	name = "web test two"
+	site = data.appgatesdp_site.default_site.id
+	tags = []
+
+	actions {
+		action = "allow"
+		hosts = [
+		"appgate.com",
+		]
+		ports = [
+		"443",
+		"80",
+		]
+		subtype = "tcp_up"
+	}
+}
 resource "appgatesdp_policy" "test_policy" {
     name  = "%s"
     notes = "terraform policy notes"
@@ -59,6 +187,45 @@ resource "appgatesdp_policy" "test_policy" {
     entitlement_links = [
         "developer"
     ]
+    ringfence_rule_links = [
+        "developer"
+    ]
+    tamper_proofing = true
+	entitlements = [
+		appgatesdp_entitlement.one.id,
+		appgatesdp_entitlement.two.id,
+	]
+    proxy_auto_config {
+        enabled = true
+        url     = "http://foo.com"
+        persist = false
+    }
+    trusted_network_check {
+        enabled    = true
+        dns_suffix = "aa"
+    }
+}
+
+`, rName)
+}
+
+// Test for removing entitlements list and links
+// https://github.com/appgate/terraform-provider-appgatesdp/issues/308
+func testAccCheckPolicyBasicUpdated(rName string) string {
+	return fmt.Sprintf(`
+
+resource "appgatesdp_policy" "test_policy" {
+    name  = "%s"
+    notes = "terraform policy notes"
+    tags = [
+        "terraform",
+        "api-created"
+    ]
+    disabled = false
+
+    expression = <<-EOF
+        return true;
+    EOF
     ringfence_rule_links = [
         "developer"
     ]
