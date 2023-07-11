bf0ca3f54f244bf44c735b968de5896b491f9756
Masaya Ando <masaya.ando@appgate.com>
2023-07-11 11:04:17 -0400

    Fix constraints to run test only for 6.0<

--- appgate/resource_appgate_global_settings_test.go
+++ appgate/resource_appgate_global_settings_test.go
@@ -142,7 +142,7 @@ func TestAccGlobalSettings6AndAbove(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				PreConfig: func() {
-					applianceConstraintCheck(t, ">= 5.5, < 6.2")
+					applianceConstraintCheck(t, ">= 6.0, < 6.2")
 				},
 				Config: `
 				resource "appgatesdp_global_settings" "test_global_settings" {
